294cfb2e2001b14d198e614019d132f95aac5fa3
RREYQH <rahul.reddy.katiki.reddy@scania.com>
2022-01-24 03:05:55 +0100

    added basic java application v1

---
+++ hello-app/src/main/java/com/sinch/devops/helloapp/controllers/HelloWorldController.java
@@ -0,0 +1,41 @@
+package com.sinch.devops.helloapp.controllers;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/status")
+    public String status()
+    {
+        return "It is Up !!!";
+    }
+
+    public static final String MESSAGE_KEY = "message";
+    public static final String HOSTNAME_KEY = "hostname";
+    public static final String IP_KEY = "ip";
+    public static final String VERSION = "version";
+
+    @GetMapping(path = "/", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    public Map<String, String> helloWorld() throws UnknownHostException {
+        return getResponse();
+    }
+
+    private Map<String, String> getResponse() throws UnknownHostException {
+        String host = InetAddress.getLocalHost().getHostName();
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        Map<String, String> response = new HashMap<>();
+        response.put(MESSAGE_KEY, "Hello World!");
+        response.put(HOSTNAME_KEY, host);
+        response.put(IP_KEY, ip);
+        response.put(VERSION, "v1");
+        return response;
+    }
+}
